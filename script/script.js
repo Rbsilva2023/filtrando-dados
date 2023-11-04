8bf13dfd23459409e3d75ac3565a37df2701391b
Rbsilva2023 <robsonchristopher2022@gmail.com>
2023-11-03 23:47:42 -0300

    tarefa 3 finalizada

--- script/script.js
+++ script/script.js
@@ -1,31 +1,35 @@
 //Desenvolva sua lógica aqui utlizando o método de array filter e verifique o resultado no console do seu navegador.
 
 //1) Crie uma função para filtrar os hotéis que estão localizados no Brasil
-function filterCountry(array) {
-  return;
-}
-console.log(filterCountry(data));
+const filterCountry = data.filter((element) => {
+  return element.country === "Brasil"
+})
+
+console.log(filterCountry);
 
 // 2) Crie uma função para filtrar os hotéis que possuem o preço igual ou maior que R$200,00.
-function filterPrice(array) {
-  return;
-}
-console.log(filterPrice(data));
+const filterPrice = data.filter((element) => {
+  const bigPrice = 200
+  return element.price >= bigPrice;
+})
+console.log(filterPrice);
 
 //3) Crie uma função para filtrar os hotéis que estão abertos nesse momento (isOpen)
-function filterIsOpen(array) {
-  return;
-}
-console.log(filterIsOpen(data));
+const filterIsOpen = data.filter((element) => {
+  const open = true
+  return element.isOpen === open;
+})
+console.log(filterIsOpen);
 
 //DESAFIO
 //4) Crie uma função que filtre apenas o hotel com o nome “Copacabana Palace”. Após isso, crie outra função para verificar as datas disponíveis para agendamento nesse hotel (isAvaliable).
-function filterHotelName(array) {
-  return;
-}
+const filterHotelName = data.filter ((element) => {
+  
+  return element.name === "Copacabana Palace"
 
-function filterToBook() {
-  const hotel = filterHotelName(data);
-  return;
-}
-console.log(filterToBook());
+})
+
+console.log(filterHotelName);
+
+
+console.log(filterToBook);
